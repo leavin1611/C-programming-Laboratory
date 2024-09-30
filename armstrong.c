99523b3dc55153831a4c0a05ab1f39488e832f12
leavin1611 <leavin1611@gmail.com>
2024-09-30 15:08:53 +0530

    Add files via upload

---
+++ armstrong.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,c,temp,sum=0;
+    scanf("%d",&n);
+   temp=n;
+  while(n>0)
+  {
+      r=n%10;
+      c=r*r*r;
+      sum=c+sum;
+     n=n/10;
+
+  }
+  if(sum==temp)
+  {
+      printf("%d is an Armstrong number.",temp);
+  }
+ else
+ {
+     printf("%d is not an Armstrong number.",temp);
+ }
+
+}
+
